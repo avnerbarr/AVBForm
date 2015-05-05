9a27745a890d48a24ff6d3bd8e2c08f9f9c2cacb
Avner <avner@skygiraffe.com>
2015-05-05 15:35:21 +0300

    Initial Commit

---
+++ AVBForm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVBForm
+//
+//  Created by Avner on 5/5/15.
+//  Copyright (c) 2015 Avner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
